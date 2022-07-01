f2ba77f0e6edcbb4225c6522ee99ba8a777a0f3d
AnneKiama <kiamamaryanne@gmail.com>
2022-07-01 12:56:45 -0700

    UPPERCASE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints uppercase letters.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
