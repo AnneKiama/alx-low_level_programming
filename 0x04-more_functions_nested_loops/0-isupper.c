45f485f6f4d06700d6a32a62284a8553624ac631
AnneKiama <kiamamaryanne@gmail.com>
2022-07-07 11:40:54 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * main - check for uppercase character.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+
+	return (0);
+}
